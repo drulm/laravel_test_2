35bbea0facd29da4b48bccfe837b5d7c82153cfc
Darrell Ulm <darrellulm@gmail.com>
2017-08-07 17:07:25 -0400

    Add / edit / index for tasks list

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'TasksController@index');
+
+Route::get('/task','TasksController@add');
+Route::post('/task','TasksController@create');
+
+Route::get('/task/{task}','TasksController@edit');
+Route::post('/task/{task}','TasksController@update');
